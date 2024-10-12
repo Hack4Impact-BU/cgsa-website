e67d5baf878035fdfdcc9263bf39e480d4c2bafe
Sadid Ethun <ethun2004@gmail.com>
2024-10-12 17:32:56 -0400

    Added contact us page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
-import Header from './components/header/header'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/header/header';
+import ContactUs from './pages/contact-us/contact-us';
 
 function App() {
   return (
-    <>
-      <Header/>
-    </>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/contact-us" element={<ContactUs />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/contact-us/contact-us.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './contact-us.css';
+
+function ContactUs() {
+
+    return (
+        <div className='wrapper'>
+            <div className='contactContainer'>
+                <h1 className='mainText'>There are lots of ways to get in touch with us!</h1>
+                <div className='infoItem'>
+                    <p className='infoText'>George Sherman Union <br />
+                        775 Commonwealth Ave. <br />
+                        Basement floor <br />
+                        Boston, MA 02215</p>
+                </div>
+                <div className='infoItem'>
+                    <p className='infoText'>Mailing address: <br />
+                        1 University Way <br />
+                        c/o Student Activities Office <br />
+                        Boston, MA 02215</p>
+                </div>
+                <div className='infoItem'>
+                    <p className='infoText'>Phone: (617) 358-5575 <br />
+                        Email: cgsa@bu.edu</p>
+                </div>
+                <div className='infoItem'>
+                    <p className='infoText'>Find us on social media on Twitter, Facebook, and Instagram!</p>
+                </div>
+                <button className='contactButton'>
+                    Click here to contact us!
+                </button>
+            </div>
+        </div>
+    );
+
+}
+
+export default ContactUs;
