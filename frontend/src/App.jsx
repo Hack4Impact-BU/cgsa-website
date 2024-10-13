b810234f28da938771c4d29278462bedcc89b9ef
adam-godel <agodel@bu.edu>
2024-10-12 21:00:19 -0400

    Completed header & footer components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header/header';
+import Footer from './components/footer/footer';
 import ContactUs from './pages/contact-us/contact-us';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/contact-us" element={<ContactUs />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
