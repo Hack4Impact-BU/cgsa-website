09bf0ad84d5a074b773fd0ca890ef44b97975580
adam-godel <agodel@bu.edu>
2024-10-18 16:34:48 -0400

    Added forms (without full questions)

--- frontend/src/pages/newsletter/newsletter.jsx
+++ frontend/src/pages/newsletter/newsletter.jsx
@@ -1,5 +1,30 @@
+import React from 'react'
+import './newsletter.css'
+
 function Newsletter() {
-    return
+    return (
+        <>
+        <div className="form">
+            <h1>Newsletter Sign-Up</h1>
+            <p id="description">Placeholder text here.</p>
+            <div className="questions">
+                <div className="name">
+                    <p>First Name</p>
+                    <p>Last Name</p>
+                    <input type='text'/>
+                    <input type='text'/>
+                </div>
+                <div className="email">
+                    <p>BU Email</p>
+                    <input type='text'/>
+                </div>
+            </div>
+            <div className='center'>
+                <button className='submit'>Submit</button>
+            </div>
+        </div>
+        </>
+    )
 }
 
 export default Newsletter
--- frontend/src/pages/volunteer/volunteer.jsx
+++ frontend/src/pages/volunteer/volunteer.jsx
@@ -1,5 +1,30 @@
+import React from 'react'
+import './volunteer.css'
+
 function Volunteer() {
-    return
+    return (
+        <>
+        <div className="form">
+            <h1>Volunteer Sign-Up</h1>
+            <p id="description">Placeholder text here.</p>
+            <div className="questions">
+                <div className="name">
+                    <p>First Name</p>
+                    <p>Last Name</p>
+                    <input type='text'/>
+                    <input type='text'/>
+                </div>
+                <div className="email">
+                    <p>BU Email</p>
+                    <input type='text'/>
+                </div>
+            </div>
+            <div className='center'>
+                <button className='submit'>Submit</button>
+            </div>
+        </div>
+        </>
+    )
 }
 
 export default Volunteer
