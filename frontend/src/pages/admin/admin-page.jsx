47933ee8cdf9e3a783eec8595dcf88dec53acfbe
Sadid Ethun <ethun2004@gmail.com>
2024-11-03 18:19:15 -0500

    Added form data tables

--- frontend/src/pages/admin/admin-page.jsx
+++ frontend/src/pages/admin/admin-page.jsx
@@ -38,6 +38,81 @@ function AdminPage() {
                 />
             </div>
             <button className="form_submit">Submit</button>
+
+            <div className="tables_container">
+                <h2 className="table_header">Newsletter Sign-Up Data</h2>
+                <table className="data_table">
+                    <thead>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>BU Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td><td></td><td></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <h2 className="table_header">Space Booking Form Data</h2>
+                <div className="scrollable_container">
+                    <table className="data_table">
+                        <thead>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>BU Email</th>
+                                <th>Booking for Club</th>
+                                <th>Primary Contact Name</th>
+                                <th>Primary Contact Email</th>
+                                <th>Purpose of Booking</th>
+                                <th>Time of Booking</th>
+                                <th>Recurring Event</th>
+                                <th>Space Needed</th>
+                                <th>Closed During Booking</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td><td></td><td></td>
+                                <td></td><td></td><td></td>
+                                <td></td><td></td><td></td>
+                                <td></td><td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+
+                <h2 className="table_header">Volunteer Sign-Up Data</h2>
+                <div className="scrollable_container">
+                    <table className="data_table">
+                        <thead>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>BU Email</th>
+                                <th>Pronouns</th>
+                                <th>Graduation Year</th>
+                                <th>Phone Number</th>
+                                <th>Interested in Events</th>
+                                <th>Interested in Organizing Library</th>
+                                <th>How to Keep Space Safe</th>
+                                <th>Questions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td><td></td><td></td>
+                                <td></td><td></td><td></td>
+                                <td></td><td></td><td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 }
