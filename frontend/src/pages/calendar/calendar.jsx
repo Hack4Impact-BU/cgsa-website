0a5e259b3c05d08db7a74be1458f07adc39fa171
adam-godel <agodel@bu.edu>
2024-11-08 09:31:55 -0500

    Added form submission page and calendar embed

--- frontend/src/pages/calendar/calendar.jsx
+++ frontend/src/pages/calendar/calendar.jsx
@@ -2,6 +2,7 @@ function Calendar() {
     return (
         <>
             <h1 class="page_title">Calendar</h1>
+            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&bgcolor=%23ded7f5&showPrint=0&showCalendars=0&showTz=0&title=BU%20CGSA%20Events&src=Y19kOWI0MWM5NjA1NDgxYmMzYzJlM2EwMjg3NWNkNWFmNDVmYmFiYjNkMTMzMDYzYWFiOTUxNGVmNGZjZTZkMGMxQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23D81B60" style={{border:"solid 1px #777"}} width="800" height="600" frameBorder="0"></iframe>
         </>
     )
 }
