030107173b606e104b604b8a26dd265d46a8d9a0
adam-godel <agodel@bu.edu>
2024-11-14 22:42:27 -0500

    Added working links to search

--- frontend/src/components/menu/menu.jsx
+++ frontend/src/components/menu/menu.jsx
@@ -4,7 +4,7 @@ import './menu.css';
 function Menu({ visible }) {
   const [results, setResults] = useState([])
   const [body, setBody] = useState([])
-  const [loading, setLoading] = useState(false)
+  const links = new Map([['About Us', 'about-us'], ['Blog', 'blog'], ['Contact Us', 'contact-us'], ['Home', 'home'], ['Newsletter Sign-Up', 'newsletter'], ['Resources', 'resources'], ['Space Booking Form', 'space-booking'], ['Volunteer Sign-Up', 'volunteer']])
 
   useEffect(() => {
     fetch('http://127.0.0.1:5000/text')
@@ -15,31 +15,28 @@ function Menu({ visible }) {
   }, [])
 
   function handleSearch(e) {
-    const query = e.target.value
+    const query = e.target.value.trim()
 
     if (!e.target.value.trim()) {
       setResults([])
       return
     }
 
-    setLoading(true)
-
     try {
       let newResults = []
       body.forEach(obj => {
         const words = obj.text.split(" ")
         for (let i = 0; i < words.length; i++) {
-          console.log(words[i],  words[i].includes(query) || query.includes(words[i]))
-          if (words[i].includes(query) || query.includes(words[i]))
+          if (query.includes(" ") ? query.includes(words[i]) : words[i].includes(query))
             newResults.push({id: newResults.length, page: obj.name, excerpt: i-2 <= 0 ? words.slice(0, i+3).join(' ')+'...' : i+3 >= words.length ? '...'+words.slice(i-2, words.length).join(' ') : '...'+words.slice(i-2, i+3).join(' ')+'...'})
+          if (query.includes(" "))
+            i += query.split(" ").length-1
         }
       })
       setResults(newResults)
     } catch (error) {
       console.error('Search error:', error)
       setResults([])
-    } finally {
-      setLoading(false)
     }
   };
 
@@ -67,14 +64,13 @@ function Menu({ visible }) {
               placeholder="Search site..."
               onChange={(e) => handleSearch(e)}
             />
-            {loading && <p>Loading results...</p>}
             {results.length > 0 && (
               <div id="search-results">
                 <ul>
                   {results.map((result) => (
                     <li key={result.id}>
-                      <a href={`/`}>
-                        <p>{result.page}</p>
+                      <a href={'/'+links.get(result.page)}>
+                        <p style={{fontWeight: 'bold'}}>{result.page}</p>
                         <i style={{fontSize: '0.8rem'}}>{result.excerpt}</i>
                       </a>
                     </li>
@@ -82,7 +78,7 @@ function Menu({ visible }) {
                 </ul>
               </div>
             )}
-            {results.length === 0 && !loading && <p>No results found</p>}
+            {results.length === 0 && <p>No results found</p>}
           </div>
         </div>
       )}
