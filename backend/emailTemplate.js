cfb0982a21040416adf4603a36c00c23e5da4fdb
Sadid Ethun <ethun2004@gmail.com>
2024-12-22 22:07:28 -0500

    updated newsletter template

--- backend/emailTemplate.js
+++ backend/emailTemplate.js
@@ -15,20 +15,11 @@ const createHtmlTemplate = (title, author, content) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Newsletter</title>
         <style>
-        @font-face {
-                font-family: WhitneyBook;
-                src: url('fonts/whitneybook.ttf');
-            }
-
-            @font-face {
-                font-family: WhitneyMedium;
-                src: url('fonts/whitneymedium.ttf');
-            }
-        
             @import url('https://fonts.googleapis.com/css2?family=Libre+Bodoni:ital,wght@0,400..700;1,400..700&display=swap');
-            @import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap');
+            @import url('https://fonts.googleapis.com/css2?family=Libre+Bodoni:ital,wght@0,400..700;1,400..700&family=Lora:ital,wght@0,400..700;1,400..700&display=swap');
+
             body {
-                font-family: Lora, sans-serif;
+                font-family: 'Lora', sans-serif;
                 margin: 0;
                 padding: 0;
                 background-color: rgba(222, 215, 245, 1);
@@ -36,7 +27,6 @@ const createHtmlTemplate = (title, author, content) => {
                 flex-direction: column;
                 justify-content: center;
                 align-items: center;
-                font-family: Lora;
             }
             .container {
                 max-width: 600px;
@@ -44,27 +34,19 @@ const createHtmlTemplate = (title, author, content) => {
                 margin-left: 10px;
                 background: white;
                 overflow: hidden;
-                box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
             }
             .header {
                 color: black;
-                padding: 0px 30px 0px 30px;
-            }
-            .header img {
-                width: 100px;
-                margin-bottom: 10px;
-            }
-            .content {
-                padding: 0px 30px 0px 30px;
-                text-align: center;
+                padding: 0px;
             }
             .section {
                 padding: 15px;
                 border-bottom: dashed 0.2rem rgba(115, 82, 212, 1);
                 padding-bottom: 30px;
+                text-align: center;
             }
-            .section:last-child {
-                border-width: 0px;
+            .section:last-of-type {
+                border-bottom: none;
             }
             
             .newsletter {
@@ -73,19 +55,22 @@ const createHtmlTemplate = (title, author, content) => {
             .footer {
                 background-color: #f1f1f1;
                 text-align: center;
-                padding: 20px;
-                font-size: 12px;
+                font-size: 1rem;
                 color: #666;
+                max-width: 600px;
+                margin-right: 10px;
+                margin-left: 10px;
+                overflow: hidden;
             }
             a {
                 color: #3498db;
                 text-decoration: none;
             }
             h1, h2 {
-                font-family: Libre Bodoni, serif;
+                font-family: 'Libre Bodoni', sans-serif;
             }
             h1 {
-                font-size: 1.4rem;
+                font-size: 1.2rem;
             }
             h2 {
                 font-size: 1.2rem;
@@ -93,25 +78,28 @@ const createHtmlTemplate = (title, author, content) => {
                 margin-bottom: 0.5rem;
             }
             p {
-                font-size: 1.2rem;
+                font-size: 1.0rem;
+            }
+            img {
+                max-width: 400px;
             }
             .social-icons img {
                 width: 24px;
                 margin: 0 5px;
             }
             .executive-board {
-                font-size: 14px;
                 line-height: 1.5;
                 background-color: rgba(222, 215, 245, 0.3);
                 margin: 30px 0px 30px 0px;
                 padding-bottom: 15px;
+                border-bottom: none;
             }
             .executive-board p {
                 margin: 0;
                 font-size: 1rem;
             }
             .title {
-                font-family: WhitneyBook;
+                font-family: 'Libre Bodoni', sans-serif;
                 font-size: 1.4rem;
                 padding: 0px 15px 0px 15px;
             }
@@ -130,20 +118,20 @@ const createHtmlTemplate = (title, author, content) => {
             </div>
             
             <!-- Welcome Section -->
-            <div class="content">
+            
                 <div class="section newsletter">
                     <h1>${title}</h1>
-                    <p><strong>${author}</strong></p>
+                    <p><em>${author}</em></p>
                     ${formattedContent}
                 </div>
                 <!-- Other sections remain unchanged -->
-            </div>
+            
 
                 <!-- Volunteer Section -->
                 <div class="section">
                     <h1>Help us keep the CGSA running smoothly!</h1>
                     <p>Volunteers are super important to the CGSA, and help us make sure everything runs smoothly during the semester. We are really grateful for your help in keeping the space open so that we can provide resources to students!</p>
-                    <img src="./frontend/src/assets/volunteer.png" alt="Volunteers Needed">
+                    <img src="https://i.imgur.com/yvyEC8q.png" alt="Volunteers Needed">
                 </div>
 
                 <!-- Social Media Section -->
@@ -151,8 +139,8 @@ const createHtmlTemplate = (title, author, content) => {
                     <h1>Keeping Up with CGSA</h1>
                     <p>Follow us on social media to stay updated with our events!</p>
                     <div class="social-icons">
-                        <img src="./frontend/src/assets/instagram.png" alt="Instagram">
-                        <img src="./frontend/src/assets/Twitter.png" alt="Twitter">
+                        <a href="https://www.instagram.com/bucgsa" target="_blank"><img src="https://i.imgur.com/rEiuiC7.png" alt="Instagram"></a>
+                        <a href="https://x.com/bucgsa" target="_blank"><img src="https://i.imgur.com/iUncriW.png" alt="Twitter"></a>
                     </div>
                 </div>
 
